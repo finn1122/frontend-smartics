429442789e7dae91e4cdf585e4bd77a1f559f2a5
aspire <you@example.com>
2025-02-28 21:42:31 -0600

    main, se modifico public path de vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/'
-
+  publicPath: process.env.VUE_APP_PUBLIC_PATH || '/'
 })
